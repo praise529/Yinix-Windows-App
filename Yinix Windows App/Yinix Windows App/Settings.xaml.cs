25fb0bf492e0c0c9322c2b9db363c0c6cbd2be2c
Praise Olatope <aanuolatope@gmail.com>
2026-02-20 16:33:14 -0700

    Added Navigation, Side bar and Top Bar

--- Yinix Windows App/Yinix Windows App/Settings.xaml.cs
+++ Yinix Windows App/Yinix Windows App/Settings.xaml.cs
@@ -21,13 +21,13 @@ namespace Yinix_Windows_App;
 /// <summary>
 /// An empty page that can be used on its own or navigated to within a Frame.
 /// </summary>
-public sealed partial class MainPage : Page
+public sealed partial class Settings : Page
 {
-    public MainPage()
+    public Settings()
     {
         InitializeComponent();
     }
-    
+
     private void Navigate_Back(object sender, RoutedEventArgs e)
     {
         if (this.Frame.CanGoBack)
